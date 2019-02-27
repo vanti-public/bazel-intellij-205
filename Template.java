d2f504def593e232508f43826e109bed2f314961
Matt Nathan <matt.nathan@vanti.co.uk>
2019-02-27 11:51:51 +0000

    Add missing Template.java file, grr

---
+++ Template.java
@@ -0,0 +1,10 @@
+package {PACKAGE};
+
+public class {CLASS_NAME} {
+  /**
+   * This comment is expected {COMMENT} in IDEA's sources
+   */
+  public static void main(final String... args) {
+    System.out.println("Hello world!");
+  }
+}
